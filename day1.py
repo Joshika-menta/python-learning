c081744b0db90c92a74613d89832430af55112d8
Joshika-menta <mentajoshika123@gmail.com>
2026-08-19 00:14:30 +0530

    Add basic Python variables and employee details program

---
+++ day1.py
@@ -0,0 +1,26 @@
+#day1 of python learning(basic variables & input)
+"""
+str="joshika"
+integer=1
+double=10.00
+float=10.0
+print(str)
+print(integer)
+print(double)
+print(float)
+
+#datatypes
+print(type(str))
+print(type(integer))
+print(type(double))
+print(type(float))
+"""
+
+#basic program to print employee details
+name=input("enter employee name:")
+id=int(input("enter employee id:"))
+salary=input("enter employee salary:")
+print("employee name:",name)
+print("employee id:",id)
+print(" employee salary:",salary)
+
